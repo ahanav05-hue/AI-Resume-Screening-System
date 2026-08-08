c08e09c2e4333a1c33a4396fcfaebc8cce2da8f1
ahanav05-hue <ahanav05@gmail.com>
2026-08-08 11:00:55 +0530

    Remove NLTK dependency

--- utils/matcher.py
+++ utils/matcher.py
@@ -18,6 +18,7 @@ import re
 import nltk
 
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+STOP_WORDS = ENGLISH_STOP_WORDS
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
